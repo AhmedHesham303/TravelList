85ab4230372b6c96d0303411134a5043f80402c7
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-05-01 00:54:39 +0300

    splitting packingList component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import Logo from "../Components/Logo";
 import Form from "../Components/Form";
 import { useState } from "react";
+import { PackingList } from "./PackingList";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -44,7 +45,7 @@ export default function App() {
   );
 }
 
-function Item({ item, onDeleteItem, onToggleItem }) {
+export function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
       <input
@@ -57,44 +58,6 @@ function Item({ item, onDeleteItem, onToggleItem }) {
       </span>
       <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
-  );
-}
-function PackingList({ items, onDeleteItem, onToggleItem, clearList }) {
-  const [sortBy, setSortBy] = useState("input");
-  let sortedItems;
-  if (sortBy === "input") sortedItems = items;
-  if (sortBy === "description") {
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-  }
-  if (sortBy === "packed") {
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-  }
-  return (
-    <div className="list">
-      <ul>
-        {sortedItems.map((item) => (
-          <Item
-            item={item}
-            key={item.id}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
-            clearList={clearList}
-          />
-        ))}
-      </ul>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={clearList}>Clear List</button>
-      </div>
-    </div>
   );
 }
 function Stats({ items }) {
