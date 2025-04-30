a104503077b3142fd442b41988fc84687ecf7d04
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-04-30 20:52:01 +0300

    implementing sorting based on deffrent factors logic

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { it } from "@faker-js/faker";
 import { useState } from "react";
 
 const initialItems = [
@@ -90,10 +91,23 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -103,10 +117,10 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
         ))}
       </ul>
       <div className="actions">
-        <select>
-          <option value="input">sort by input order</option>
-          <option value="description">sort by description</option>
-          <option value="packed">sort by packed status</option>
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
         </select>
       </div>
     </div>
