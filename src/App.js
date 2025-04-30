d43931017700cc31322ee383dc1db5ed9d638c33
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-04-30 18:20:01 +0300

    adding percentage in seperate derived state

--- src/App.js
+++ src/App.js
@@ -108,11 +108,12 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 function Stats({ items }) {
   const numItems = items.length;
   const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
   return (
     <footer className="stats">
       <em>
         💼 you have {numItems} items in your list , and you already packed{" "}
-        {packedItems}({(packedItems / numItems) * 100}%)
+        {packedItems}({percentage}%)
       </em>
     </footer>
   );
