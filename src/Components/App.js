06139c9d1e21a8b8fca6f3dca45e27a6a8be62f9
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-05-01 00:47:44 +0300

    splitting form component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,5 @@
 import Logo from "../Components/Logo";
+import Form from "../Components/Form";
 import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -40,43 +41,6 @@ export default function App() {
       />
       <Stats items={items} />
     </div>
-  );
-}
-
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handelSubmit(e) {
-    e.preventDefault();
-    if (!description) return;
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    onAddItems(newItem);
-
-    setDescription("");
-    setQuantity(1);
-  }
-  return (
-    <form className="add-form" onSubmit={handelSubmit}>
-      <h3>What do you need for your 😍 trip</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="item"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      ></input>
-      <button>Add</button>
-    </form>
   );
 }
 
